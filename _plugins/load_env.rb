2bb81e3a1da281f3218b385262d1a51fe1d3f163
daffadevhosting <endsae83@gmail.com>
2025-07-22 14:48:09 +0700

    first commit

---
+++ _plugins/load_env.rb
@@ -0,0 +1,13 @@
+require 'dotenv'
+Dotenv.load
+
+Jekyll::Hooks.register :site, :after_init do |site|
+  site.config['FIREBASE_API_KEY'] = ENV['FIREBASE_API_KEY']
+  site.config['FIREBASE_AUTH_DOMAIN'] = ENV['FIREBASE_AUTH_DOMAIN']
+  site.config['FIREBASE_PROJECT_ID'] = ENV['FIREBASE_PROJECT_ID']
+  site.config['FIREBASE_APP_ID'] = ENV['FIREBASE_APP_ID']
+  site.config['PAYPAL_CLIENT_ID'] = ENV['PAYPAL_CLIENT_ID']
+  site.config['MIDTRANS_CLIENT_KEY'] = ENV['MIDTRANS_CLIENT_KEY']
+  site.config['MIDTRANS_URL'] = ENV['MIDTRANS_URL']
+  site.config['GEMINI_API'] = ENV['GEMINI_API']
+end
