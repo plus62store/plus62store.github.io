40a3c7ba4ebd993ecade8b1b3fcbc85b99d3257e
daffadevhosting <endsae83@gmail.com>
2025-07-24 02:21:56 +0700

    update all

--- _plugins/load_env.rb
+++ _plugins/load_env.rb
@@ -10,4 +10,5 @@ Jekyll::Hooks.register :site, :after_init do |site|
   site.config['MIDTRANS_CLIENT_KEY'] = ENV['MIDTRANS_CLIENT_KEY']
   site.config['MIDTRANS_URL'] = ENV['MIDTRANS_URL']
   site.config['GEMINI_API'] = ENV['GEMINI_API']
+  site.config['WEB_ID'] = ENV['WEB_ID']
 end
