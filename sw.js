551ea5d7fa192aef2e0919ea51ff80dd052a3970
plus62store <plus62store@users.noreply.github.com>
2026-09-13 16:42:47 +0000

    deploy: a1a5a6f073139fd5ecf8a63efbf7de94a64277de

--- sw.js
+++ sw.js
@@ -35,10 +35,10 @@ workbox.precaching.precacheAndRoute([
   { url: '/product/masker-bordir-tasik-ilalang/', revision: '2021-02-02' },
   { url: '/product/masker-bordir-tasik-dahlia/', revision: '2021-02-01' },
   { url: '/product/mukena-garcella-rayon-premium/', revision: '2020-05-22' },
-  { url: '/', revision: '202609131637' },
-  { url: '/product', revision: '202609131637' },
-  { url: '/assets/css/style.css', revision: '202609131637' },
-  { url: '/assets/css/webshop.css', revision: '202609131637' }
+  { url: '/', revision: '202609131642' },
+  { url: '/product', revision: '202609131642' },
+  { url: '/assets/css/style.css', revision: '202609131642' },
+  { url: '/assets/css/webshop.css', revision: '202609131642' }
 ])
 
 registerRoute(
