6fe3098c356ffc335ef13d8004bbba94879f1eb8
plus62store <plus62store.info@gmail.com>
2026-09-20 04:08:09 +0700

    Remove deprecated JavaScript files and update style-picker functionality to enhance user experience

--- assets/js/style-picker.js
+++ assets/js/style-picker.js
@@ -1,5 +1,18 @@
-jQuery('.style-picker div').click(function() {
-  var target = $(this).attr('id');
-  $(this).addClass('item_color').siblings().removeClass('item_color');
-  $('#' + target).show().siblings('div').hide();
+$(function () {
+  $('.style-picker div').on('click', function () {
+    var target = $(this).attr('id');
+    var image = $(this).data('image');
+
+    $(this).addClass('item_color').siblings().removeClass('item_color');
+    $('#' + target).show().siblings('div').hide();
+
+    if (image) {
+      $('.simpleCart_shelfItem .item_thumb').attr('src', image);
+    }
+  });
+
+  var selectedStyle = $('.style-picker div.item_color').first();
+  if (selectedStyle.length && selectedStyle.data('image')) {
+    $('.simpleCart_shelfItem .item_thumb').attr('src', selectedStyle.data('image'));
+  }
 });
